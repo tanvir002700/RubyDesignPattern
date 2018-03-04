3301a8d38bfce3d782bb26ff30a392a15aa3381f
tanvir hasan <tanvir002700@gmail.com>
2018-03-04 23:39:17 +0600

    initialize singleton method

---
+++ singleton_pattern.rb
@@ -0,0 +1,15 @@
+class Logger
+  attr_reader :log
+
+  def initialize
+    @log = 'initialize logger'
+  end
+
+  @@instance = Logger.new
+
+  def self.instance
+    @@instance
+  end
+
+  private_class_method :new
+end
