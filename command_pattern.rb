80856e0a0982bcd749562135b744529cd4cf5f53
tanvir hasan <tanvir002700@gmail.com>
2018-03-08 22:31:05 +0600

    implement the command design pattern

--- command_pattern.rb
+++ command_pattern.rb
@@ -14,3 +14,61 @@ class Television
     puts "Tv is Off"
   end
 end
+
+module Command
+  def execute; end
+  def undo; end
+end
+
+class TurnTvOn
+  include Command
+
+  def initialize(newDevice)
+    @theDevice = newDevice
+  end
+
+  def execute
+    @theDevice.on
+  end
+
+  def undo
+    @theDevice.off
+  end
+end
+
+class TurnTvOff
+  include Command
+
+  def initialize(newDevice)
+    @theDevice = newDevice
+  end
+
+  def execute
+    @theDevice.off
+  end
+
+  def undo
+    @theDevice.on
+  end
+end
+
+class DeviceButton
+  def initialize(new_command)
+    @the_command = new_command
+  end
+
+  def press
+    @the_command.execute
+  end
+
+  def press_undo
+    @the_command.undo
+  end
+end
+
+class TVRemote
+  def self.getDevice
+    new.Television
+  end
+end
+
