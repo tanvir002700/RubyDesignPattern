dfe9bd92a2cf427696efa17995de0125d91e7772
tanvir hasan <tanvir002700@gmail.com>
2018-03-08 23:46:45 +0600

    add new functionality in command design pattern

--- command_pattern.rb
+++ command_pattern.rb
@@ -1,17 +1,35 @@
 module ElectronicDevice
+  attr_accessor :status, :volume
+
   def on; end
   def off; end
+  def volume_up; end
+  def volume_down; end
 end
 
 class Television
   include ElectronicDevice
 
+  def initialize
+    @volume = 0
+    @status = false
+  end
+
   def on
     puts "Tv is On"
+    @status = true
   end
 
   def off
     puts "Tv is Off"
+  end
+
+  def volume_up
+    @volume += 1
+  end
+
+  def volume_down
+    @volume -= 1
   end
 end
 
@@ -20,7 +38,7 @@ module Command
   def undo; end
 end
 
-class TurnTvOn
+class TvOn
   include Command
 
   def initialize(newDevice)
@@ -36,7 +54,7 @@ class TurnTvOn
   end
 end
 
-class TurnTvOff
+class TvOff
   include Command
 
   def initialize(newDevice)
@@ -49,6 +67,34 @@ class TurnTvOff
 
   def undo
     @theDevice.on
+  end
+end
+
+class TvVolumeUp
+  def initialize(new_device)
+    @the_device = new_device
+  end
+
+  def execute
+    @the_device.volume_up
+  end
+
+  def undo
+    @the_device.volume_down
+  end
+end
+
+class TvVolumeDown
+  def initialize(new_device)
+    @the_device = new_device
+  end
+
+  def execute
+    @the_device.volume_down
+  end
+
+  def undo
+    @the_device.volume_up
   end
 end
 
@@ -67,8 +113,8 @@ class DeviceButton
 end
 
 class TVRemote
-  def self.getDevice
-    new.Television
+  def self.get_device
+    Television.new
   end
 end
 
