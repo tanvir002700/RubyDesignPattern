545fe0a3bb229f7c18d4b91caf2a819e278a8074
tanvir hasan <tanvir002700@gmail.com>
2018-03-11 00:35:03 +0600

    implement facedae pattern

--- facade_pattern.rb
+++ facade_pattern.rb
@@ -57,3 +57,26 @@ class BankFund
   end
 end
 
+class BankAccountFacade
+  attr_accessor :welcome, :account_checker, :code_checker, :fund
+  def initialize(new_account_number, new_sec_code)
+    @account_number = new_account_number
+    @sec_code = new_sec_code
+
+    @welcome = WelcomeToBank.new
+    @account_checker = AccountNumberCheck.new
+    @code_checker = SecurityCodeCheck.new
+    @fund = BankFund.new
+  end
+
+  def withdraw_cash(cash)
+    if(account_checker.account_activate?(@new_account_number) && fund.code_correct?(@sec_code))
+      if(fund.withdraw(cash))
+        puts "successfully with draw"
+      else
+        puts "something went wrong"
+      end
+    end
+  end
+end
+
