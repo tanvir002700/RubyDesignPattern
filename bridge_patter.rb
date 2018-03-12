325b8f9658d7b606b87ca2d5074d490c85acbec8
tanvir hasan <tanvir002700@gmail.com>
2018-03-12 23:05:53 +0600

    implement bridge pattern

--- bridge_patter.rb
+++ bridge_patter.rb
@@ -1,11 +1,11 @@
 module EntertainmentDevice
-  attr_reader :device_state, :max_seeting, :volume_level
+  attr_reader :device_state, :max_setting, :volume_level
 
   def button_five_pressed; end
   def button_six_pressed; end
 
   def device_feed_back
-    if device_state > max_seeting || device_state < 1
+    if device_state > max_setting || device_state < 1
       @device_state = 0
     end
   end
@@ -21,3 +21,55 @@ module EntertainmentDevice
   end
 end
 
+class TVDevice
+  include EntertainmentDevice
+
+  def initialize(device_state, max_setting)
+    @device_state = device_state
+    @max_setting = max_setting
+  end
+
+  def button_five_pressed
+    puts "channel down"
+    @device_state -= 1
+  end
+
+  def button_six_pressed
+    puts "channel up"
+    @device_state += 1
+  end
+end
+
+module RemoteButton
+  attr_reader :device
+
+  def initialize(device)
+    @device = device
+  end
+
+  def buffton_five_pressed
+    device.button_five_pressed
+  end
+
+  def button_six_pressed
+    device.button_six_pressed
+  end
+
+  def device_feed_back
+    device.device_feed_back
+  end
+
+  def button_nine_pressed; end
+end
+
+class TvRemoteMute
+  include RemoteButton
+
+  def initialize(device)
+    super(device)
+  end
+
+  def button_nine_pressed
+    puts "tv is mute"
+  end
+end
