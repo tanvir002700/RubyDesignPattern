0186fb5501d6811001011e61e20fb0d921152528
tanvir hasan <tanvir002700@gmail.com>
2018-03-21 23:02:23 +0600

    implement class for proxy

--- proxy_pattern.rb
+++ proxy_pattern.rb
@@ -2,3 +2,24 @@ module GetAtmData
   def get_atm_state; end
   def get_cache_in_machine; end
 end
+
+class AtmMachine
+  include GetAtmData
+
+  def get_yes_card_state
+    :has_card
+  end
+
+  def get_no_card_state
+    :no_card
+  end
+
+  def get_has_pin
+    :has_correct_pin
+  end
+
+  def get_no_cache_state
+    :atm_out_of_money
+  end
+end
+
