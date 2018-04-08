8f62012b33fe48ed2a512d714431336952073312
tanvir hasan <tanvir002700@gmail.com>
2018-04-08 21:02:12 +0600

    Add hero and computer class

---
+++ Structural pattern/proxy/computer.rb
@@ -0,0 +1,39 @@
+class Computer
+  def initialize
+    @queue = []
+  end
+
+  def add(command)
+    @queue << command
+  end
+
+  def execute
+    'executing commands'
+  end
+end
+
+class ComputerProxy
+  extend Forwardable
+
+  def_delegators :real_object, :add
+
+  def initialize(hero)
+    @hero = hero
+  end
+
+  def execute
+    check_access
+    real_object.execute
+  end
+
+  def check_access
+    unless @hero.keywords.include?(:computer)
+      raise 'You have no access'
+    end
+  end
+
+  def real_object
+    @real_object ||= Computer.new
+  end
+end
+
