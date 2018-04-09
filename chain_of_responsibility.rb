6b6224f1aec74c3bedf39f774f4fc618237f01d8
tanvir hasan <tanvir002700@gmail.com>
2018-04-09 21:11:13 +0600

    add number class

--- chain_of_responsibility.rb
+++ chain_of_responsibility.rb
@@ -3,3 +3,13 @@ module Chain
   def calculate; end
 end
 
+class Number
+  attr_accessor :number1, :number2
+  attr_reader :calcuation_wanted
+
+  def initialize(number1, number2, cal_wanted)
+    @number1 = number1
+    @number2 = number2
+    @calcuation_wanted = cal_wanted
+  end
+end
