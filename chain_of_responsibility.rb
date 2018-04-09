228aab41249053631d7ba5add329b7c945f54d47
tanvir hasan <tanvir002700@gmail.com>
2018-04-09 22:56:48 +0600

    add substract class

--- chain_of_responsibility.rb
+++ chain_of_responsibility.rb
@@ -27,3 +27,16 @@ class AddNumbers
   end
 end
 
+class SubtractNumbers
+  attr_accessor :next_chain
+
+  def calculate(request)
+    if request.calcuation_wanted == :sub
+      puts "#{request.number1} - #{request.number2}"
+      request.number1 - request.number2
+    else
+      next_chain.calculate(request)
+    end
+  end
+end
+
